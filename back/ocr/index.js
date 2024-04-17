2c1f02e3289c1ecc2c20afdba202f33293ef43ae
seon_ <108112575+seon03@users.noreply.github.com>
2024-04-18 01:43:19 +0900

    Update index.js

--- back/ocr/index.js
+++ back/ocr/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import BottomNav from './components/ui/BottomNav';
-import AccountAddModal from './components/modals/AccountAddModal';
-import ScheduleAddModal from './components/modals/ScheduleAddModal';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import { Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Routes>
-    <Route path="/ocr" element={<App />} />
-  </Routes>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
