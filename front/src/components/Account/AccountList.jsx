167c80fbbcb4c1491b397f937c87729e022f6cff
seon_ <108112575+seon03@users.noreply.github.com>
2024-04-19 04:58:47 +0900

    Update AccountList.jsx

--- front/src/components/Account/AccountList.jsx
+++ front/src/components/Account/AccountList.jsx
@@ -4,38 +4,6 @@ import AccordionItems from "./AccordionItems";
 import UnexpectedItem from "./UnexpectedItem";
 import "../../styles = css/AccountList.css"
 ;
-/* AccountList (1차)
-//import styled from "styled-components";
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-
-    :not(:last-child) {
-        margin-bottom: 16px;
-    }
-`;
-
-function ExpensesList(props){
-    const { expenses } = props;
-
-    return (
-        <Wrapper>
-            {expenses.map((expense, index) => {
-                return (
-                    <ExpensesItem 
-                        key={expense.title}
-                        expense={expense}
-                    />
-                );
-            })}
-        </Wrapper>
-    );
-}
-
-export default ExpensesList;
-*/
 
 //// Accordian items list
 function AccountList(props) {
