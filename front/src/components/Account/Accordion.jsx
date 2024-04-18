91adec11c9d8743f1ed93aa40b3336d5ee59ac06
seon_ <108112575+seon03@users.noreply.github.com>
2024-04-19 05:11:16 +0900

    Update Accordion.jsx

--- front/src/components/Account/Accordion.jsx
+++ front/src/components/Account/Accordion.jsx
@@ -6,8 +6,15 @@ import { ScheduleProvider } from '../../contexts/ScheduleContext';
 
 const Accordion = () => {
     const { accordionData } = useScheduleContext();
+    const [main, setMains] = useState([]);
     const [posts, setPosts] = useState([]);
     const [acc, setAcc] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/main')
+        .then((res) => res.json())
+        .then((res) => {setMains(res)});
+    }, [main]);
 
     useEffect(() => {
         fetch('http://localhost:8080/api/v1/schedule')
@@ -25,6 +32,9 @@ const Accordion = () => {
         <ScheduleProvider>
             <ul className="accordion">
                 {/*백엔드에서 가져온 데이터*/}
+                {main.map((main) => (
+                    <AccordionItem key={main.scheduleId} schedule={main.title} income={main.income} expesne={main.expense} />
+                ))}
                 {acc.map((acc) => (
                     <AccordionItem key={acc.schedule} schedule={acc.title} expesne={acc.cost} />
                 ))}
@@ -42,5 +52,4 @@ const Accordion = () => {
 };
 
 export default Accordion;
-
 
