87f5616832f8aed5bc4f3c8b3fdb4076443dc999
seon_ <108112575+seon03@users.noreply.github.com>
2024-04-19 04:46:32 +0900

    Update Accordion.jsx

--- front/src/components/Account/Accordion.jsx
+++ front/src/components/Account/Accordion.jsx
@@ -24,12 +24,15 @@ const Accordion = () => {
     return (
         <ScheduleProvider>
             <ul className="accordion">
+                //백엔드에서 가져온 데이터
                 {acc.map((acc) => (
                     <AccordionItem key={acc.schedule} schedule={acc.title} expesne={acc.cost} />
                 ))}
                 {posts.map((post) => (
                     <AccordionItem schedule={post.title} />
                 ))}
+
+                //flask예산예측에서 가져온 데이터
                 {accordionData.map((data, index) => (
                     <AccordionItem key={index} schedule={data.schedule_name} expected={data.prediction_result} income={data.income} expense={data.expense} details={data.details}/>
                 ))}
@@ -41,32 +44,3 @@ const Accordion = () => {
 export default Accordion;
 
 
-/* 백엔드에서 가져오기
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-const Accordian = () => {
-    const [scacs, setHello] = useState('');
-
-    useEffect(() => {
-        axios.get('?')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <ul className="accordion">
-            //백엔드에서 가져온 데이터
-            {scacs.map((scac, index) => (
-                <AccordionItem key={index} scac={scac} />
-            ))}
-            //flask에서 가져온 데이터
-            {accordionData.map((data, index) => (
-                    <AccordionItem key={index} schedule={data.schedule_name} expected={data.prediction_result} income={data.income} expense={data.expense} details={data.details}/>
-            ))}
-        </ul>
-    );
-}
-
-export default Accordian;
-
-*/
