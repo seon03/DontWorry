5e44507dbbe366ebf0f847ef4f5b0c2ded87aa64
seon_ <108112575+seon03@users.noreply.github.com>
2024-04-19 05:12:03 +0900

    Update AccordionHeader.jsx

--- front/src/components/Account/AccordionHeader.jsx
+++ front/src/components/Account/AccordionHeader.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import "../../styles = css/AccordionHeader.css";
 
 //import CustomDate from '../ui/CustomDate';
 //import MonthFilterButton from '../ui/MonthFilterButton';
 
 //백엔드에서 가져오기
-function AccordionHeader(props) {
-    const [acs, setAcs] = useState('');
-
-    useEffect(() => {
-        axios.get('/api/acs')
-        .then(response => setAcs(response.data))
-        .catch(error => console.log(error))
-    }, []);
+function AccordionHeader({scheduleDate}) {
 
     return (
         <div className="heading">
                 <div className='accountHeaderFrame'>
                     <div className='schedule-expected'>
                         <div className='오늘'>오늘</div>
-                        <div className="div-2-span" >{/*<CustomDate/>*/}4월 17일{/*<MonthFilterButton />*/}</div>
+                        <div className="div-2-span" >{scheduleDate}{/*<CustomDate/>*/}4월 17일{/*<MonthFilterButton />*/}</div>
                     </div>
                     <div className='총수입지출'>
                             <div className='total-income'>
