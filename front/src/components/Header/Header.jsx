6a6ec8f6bc62360fab6a27aa8f19000639b497f9
seon_ <108112575+seon03@users.noreply.github.com>
2024-06-19 15:59:47 +0900

    Add files via upload

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MenuButton from '../ui/MenuButton';
-import ProfileStatus from './ProfileStatus';
-import AlertButton from '../ui/AlertButton';
+//import ProfileStatus from './ProfileStatus';
+//import AlertButton from '../ui/AlertButton';
 import BudgetStatus from '../Budget/BudgetStatus';
 import "../../styles = css/Header.css"
 
@@ -11,8 +11,7 @@ function Header () {
             <header className="budget-header">
                 <div className="profile-header">
                     <MenuButton />
-                    <div><ProfileStatus /></div>
-                    <AlertButton />
+                    {/*<AlertButton />*/}
                 </div>
                 <BudgetStatus />
             </header>
